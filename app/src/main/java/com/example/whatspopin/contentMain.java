2e9b0e0a78ec1e4abc57db2292764402d65c7c78
Thanos Pourikis <thanos.pourikis@gmail.com>
2019-12-12 17:59:10 +0200

    Thanos

---
+++ app/src/main/java/com/example/whatspopin/contentMain.java
@@ -0,0 +1,13 @@
+package com.example.whatspopin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class contentMain extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.content_main);
+
+	}
+}
--- app/src/main/java/com/example/whatspopin/profileActivity.java
+++ app/src/main/java/com/example/whatspopin/profileActivity.java
@@ -1,12 +1,24 @@
 package com.example.whatspopin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class profileActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+
+		Button btn = (Button) findViewById(R.id.loginBtn);
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(v.getContext(), contentMain.class);
+				startActivityForResult(myIntent, 0);
+			}
+		});
 	}
 
 	}
