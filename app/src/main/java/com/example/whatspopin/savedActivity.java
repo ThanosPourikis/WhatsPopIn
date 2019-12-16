86183747fd842967153467517ecf4cc3bee0e679
Thanos Pourikis <thanos.pourikis@gmail.com>
2019-12-16 19:12:18 +0200

    Thanos

--- app/src/main/java/com/example/whatspopin/savedActivity.java
+++ app/src/main/java/com/example/whatspopin/savedActivity.java
@@ -3,10 +3,26 @@ package com.example.whatspopin;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.whatspopin.database.Event;
+import com.example.whatspopin.database.WhatsPopInDatabase;
+
+import java.util.List;
+
 public class savedActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.saved_events);
+		WhatsPopInDatabase db = WhatsPopInDatabase.getInstance(this);
+		List<Event> ev = db.eventDao().getEventList();
+		for(Event i :ev){
+			System.out.println(i.getName());
+			System.out.println(i.getCategory());
+			System.out.println(i.getPlace());
+			System.out.println(i.getDescription());
+
+
+
+		}
 
 	}
 }
